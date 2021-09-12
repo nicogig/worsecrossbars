5d17741c7ca5b63b70f113e2c4002a197a5b158e
Nicola Gigante <nicolagigante@outlook.com>
2021-09-12 14:14:17 +0100

    Altered weight_mapping.py

--- weight_mapping.py
+++ weight_mapping.py
@@ -1,25 +1,27 @@
 import numpy as np
 
-def choose_extremes(W, HRS_LRS_ratio, excluded_weights_proportion):
+def choose_extremes(network_weights, HRS_LRS_ratio, excluded_weights_proportion):
     """
     choose_extremes:
         Choose the minimum and maximum discrete weights
     Inputs:
-        -   W, an array containing all the continuous weights
-            in a synaptic layer.
+        -   network_weights: The weights as outputted by the training functions.
         -   HRS_LRS_ratio: the ratio desired.
         -   excluded_weights_proportion:  proportion of excluded 
             synaptic weights with the largest absolute values.
     Output:
-        - A tuple, (w_max, w_min), with the maximum and minimum 
-        discrete weights.
+        - A list of tuples, (w_max, w_min), with the maximum and minimum 
+        discrete weights in a layer.
     """
-    
-    W_abs = np.absolute(W)
-    W_abs_sort = np.argsort(-W_abs)
-    s = W_abs_sort.size
-    index = int(excluded_weights_proportion * s)
-    w_max = W_abs_sort(index)
-    w_min = w_max / HRS_LRS_ratio
-    return (w_max, w_min)
+    return_list = []
+    for list_weights in network_weights:
+        array_weights = np.array(list_weights)
+        W_abs = np.abs(array_weights)
+        W_abs_sort = np.argsort(-W_abs)
+        s = W_abs_sort.size
+        index = int(excluded_weights_proportion * s)
+        w_max = W_abs_sort(index)
+        w_min = w_max / HRS_LRS_ratio
+        return_list.append((w_max, w_min))
+    return return_list
 
