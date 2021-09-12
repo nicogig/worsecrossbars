6d5dad144399bbd087da4cec3ece495df39acbf7
Nicola Gigante <nicolagigante@outlook.com>
2021-09-12 19:00:46 +0100

    Added weight interval for all layers

--- weight_mapping.py
+++ weight_mapping.py
@@ -15,28 +15,32 @@ def choose_extremes(network_weights, HRS_LRS_ratio, excluded_weights_proportion)
     """
     
     return_list = []
-    for list_weights in network_weights:
-        array_weights = np.array(list_weights)
-        W_abs = np.abs(array_weights)
-        W_abs_sort = np.argsort(-W_abs)
-        s = W_abs_sort.size
-        index = int(excluded_weights_proportion * s)
-        w_max = W_abs_sort(index)
-        w_min = w_max / HRS_LRS_ratio
-        return_list.append((w_max, w_min))
+    for count, layer_weights in enumerate(network_weights):
+        if count % 2 == 0:
+            array_weights = np.flatten(layer_weights)
+            W_abs = np.abs(array_weights)
+            W_abs_sort = np.argsort(-W_abs)
+            s = W_abs_sort.size
+            index = int(excluded_weights_proportion * s)
+            w_max = W_abs_sort(index)
+            w_min = w_max / HRS_LRS_ratio
+            return_list.append((w_max, w_min))
     return return_list
 
 
-def create_weight_interval(w_max, w_min, no_of_weights):
+def create_weight_interval(list_of_extremes, no_of_weights):
     """
     create_weight_interval:
         Create an evenly spaced weight interval.
     Inputs:
-        -   w_max: The maximum value for the weights, as given by choose_extremes()
-        -   w_min: The minimum value for the weights, as given by choose_extremes()
+        -   list_of_extremes: A list of tuples, as returned by choose_extremes()
         -   no_of_weights: The number of weights needed.
     Output:
-        -   A list of evenly spaced weights.
+        -   A list of lists of evenly spaced weights.
     """
-    return np.linspace(w_min, w_max, no_of_weights)
+
+    return_list = []
+    for element in list_of_extremes:
+        return_list.append(np.linspace(element[1], element[0], no_of_weights))
+    return return_list
 
