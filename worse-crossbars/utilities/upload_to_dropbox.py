bfdf4c627dcdafac84dd00e3a8a4aa59cba3d6e4
Nicola Gigante <nicolagigante@outlook.com>
2021-10-01 20:08:21 +0100

    Update upload_to_dropbox.py

--- worse-crossbars/utilities/upload_to_dropbox.py
+++ worse-crossbars/utilities/upload_to_dropbox.py
@@ -1,17 +1,44 @@
 import shutil
 import os
 import dropbox
-import auth_dropbox
 import json
+from dropbox import DropboxOAuth2FlowNoRedirect
 import secrets
 
 dropbox_secrets = {}
 auth_checked = False
 
+def authenticate():
+    auth_flow = DropboxOAuth2FlowNoRedirect(secrets.APP_KEY, consumer_secret=secrets.APP_SECRET, token_access_type='offline',
+                                         scope=['account_info.read', 'files.content.read', 'files.content.write'],)
+
+    authorize_url = auth_flow.start()
+    print("1. Go to: " + authorize_url)
+    print("2. Click \"Allow\" (you might have to log in first).")
+    print("3. Copy the authorization code.")
+    auth_code = input("Enter the authorization code here: ").strip()
+
+    try:
+        oauth_result = auth_flow.finish(auth_code)
+    except Exception as e:
+        print('Error: %s' % (e,))
+        exit(1)
+
+    with dropbox.Dropbox(oauth2_access_token=oauth_result.access_token) as dbx:
+        dbx.users_get_current_account()
+        print("Successfully set up client!")
+
+    secret = {"dropbox_auth_code": oauth_result.access_token,
+              "dropbox_expiration": oauth_result.expires_at.__str__(),
+              "dropbox_refresh":oauth_result.refresh_token}
+
+    with open("./config/user_secrets.json", 'w') as outfile:
+        json.dump(secret, outfile)
+
 def check_auth_presence ():
     global auth_key, auth_checked, dropbox_secrets
     if not os.path.exists("./config/user_secrets.json"):
-        auth_dropbox.authenticate()
+        authenticate()
         check_auth_presence()
     else:
         with open("./config/user_secrets.json") as json_file:
@@ -40,4 +67,4 @@ def upload (fault_type, number_hidden_layers):
     else:
         check_auth_presence()
         upload(fault_type, number_hidden_layers)
-
+        
