ed56df2a9065d6888765880172d9219c0fc22dc8
Nicola Gigante <nicolagigante@outlook.com>
2021-10-02 14:59:53 +0100

    Update create_folder_structure.py

--- worsecrossbars/utilities/create_folder_structure.py
+++ worsecrossbars/utilities/create_folder_structure.py
@@ -6,18 +6,9 @@ def user_folders():
     """
     
     home_dir = Path.home()
-    home_dir.mkdir("worsecrossbars", parents=True, exist_ok=True)
-    
-    py_folder = home_dir.joinpath("worsecrossbars")
-    py_folder.mkdir("outputs", parents=True, exist_ok=True)
-    py_folder.mkdir("config", parents=True, exist_ok=True)
-
-    outputs_folder = py_folder.joinpath("outputs")
-    outputs_folder.mkdir("accuracies", parents=True, exist_ok=True)
-    outputs_folder.mkdir("logs", parents=True, exist_ok=True)
-    outputs_folder.mkdir("plots", parents=True, exist_ok=True)
-    outputs_folder.mkdir("training_validation", parents=True, exist_ok=True)
-    
-    plots_folder = outputs_folder.joinpath("plots")
-    plots_folder.mkdir("accuracies", parents=True, exist_ok=True)
-    plots_folder.mkdir("training_validation", parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "outputs", "accuracies").mkdir(parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "outputs", "logs").mkdir(parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "outputs", "plots", "accuracies").mkdir(parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "outputs", "plots", "training_validation").mkdir(parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "outputs", "training_validation").mkdir(parents=True, exist_ok=True)
+    home_dir.joinpath("worsecrossbars", "config").mkdir(parents=True, exist_ok=True)
