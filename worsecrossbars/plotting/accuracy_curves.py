4179b08d5dd3ce7afbc22136cdf9aec918b84b1e
Lorenzo Bonito <lol99.bonito@gmail.com>
2021-10-29 11:01:59 +0100

    Updated plotting

--- worsecrossbars/plotting/accuracy_curves.py
+++ worsecrossbars/plotting/accuracy_curves.py
@@ -18,31 +18,38 @@ accuracy_labels = []
 for noise_idx, noise in enumerate([True, False]):
 
     for number_hidden_layers in range(1, 5):
-        accuracy_labels.append(f"{number_hidden_layers} hidden layers, {noise} noise" if (number_hidden_layers != 1) else f"1 hidden layer, {noise} noise")
+
+        if noise:
+            accuracy_labels.append(f"{number_hidden_layers} hidden layers" if (number_hidden_layers != 1) else f"1 hidden layer")
 
         for fault_type in range (1, 4):
             try:
-                data[noise_idx][fault_type-1].append(pickle.load(open(str(configs.working_dir.joinpath("outputs", "accuracies", f"accuracies_faultType{fault_type}_{number_hidden_layers}HL_{noise}N.pickle")), "rb")))
+                data[noise_idx][fault_type-1].append(pickle.load(open(str(configs.working_dir.joinpath("outputs", "accuracies", f"accuracies_faultType{fault_type}_{number_hidden_layers}HL_{noise}N_1NV.pickle")), "rb")))
             except FileNotFoundError:
-                print(f"Fault {number_hidden_layers}HL_{fault_type}FT_{noise}N not yet implemented.")
+                print(f"File for fault {number_hidden_layers}HL_{fault_type}FT_{noise}N is not present.")
+
+
+data_cannot_electroform = data[1][0]
+data_cannot_electroform_noise = data[0][0]
+data_stuck_at_HRS = data[1][1]
+data_stuck_at_HRS_noise = data[0][1]
+data_stuck_at_LRS = data[1][2]
+data_stuck_at_LRS_noise = data[0][2]
 
 try:
-    data_cannot_electroform = data[1][0]
-    data_cannot_electroform_noise = data[0][0]
-    data_stuck_at_HRS = data[1][1]
-    data_stuck_at_HRS_noise = data[0][1]
-    data_stuck_at_LRS = data[1][2]
-    data_stuck_at_LRS_noise = data[0][2]
+    # Plotting accuracy curves for regular data
+    accuracy_curves_plotter(data_cannot_electroform[0][0], [data_cannot_electroform[i][1] for i in range(0,4)], fault_type=data_cannot_electroform[0][2], fpath=font, save=True, labels=accuracy_labels)
+    accuracy_curves_plotter(data_stuck_at_HRS[0][0], [data_stuck_at_HRS[i][1] for i in range(0,4)], fault_type=data_stuck_at_HRS[0][2], fpath=font, save=True, labels=accuracy_labels)
+    accuracy_curves_plotter(data_stuck_at_LRS[0][0], [data_stuck_at_LRS[i][1] for i in range(0,4)], fault_type=data_stuck_at_LRS[0][2], fpath=font, save=True, labels=accuracy_labels)
 except IndexError:
-    print("One of the necessary files is missing.")
+    print("Some necessary file is missing: complete accuracy plot cannot be produced.")
 
-# # Plotting accuracy curves for Type I (Cannot Electroform) fault
-# accuracy_curves_plotter(data_cannot_electroform[0][0], [data_cannot_electroform[i][1] for i in range(0,4)], fault_type=data_cannot_electroform[0][2], fpath=font, save=True, labels=accuracy_labels)
+try:
+    # Plotting accuracy for AWGN data
+    accuracy_curves_plotter(data_cannot_electroform_noise[0][0], [data_cannot_electroform_noise[i][1] for i in range(0,4)], fault_type=data_cannot_electroform_noise[0][2], noise=True, fpath=font, save=True, labels=accuracy_labels)
+    accuracy_curves_plotter(data_stuck_at_HRS_noise[0][0], [data_stuck_at_HRS_noise[i][1] for i in range(0,4)], fault_type=data_stuck_at_HRS_noise[0][2], noise=True, fpath=font, save=True, labels=accuracy_labels)
+    accuracy_curves_plotter(data_stuck_at_LRS_noise[0][0], [data_stuck_at_LRS_noise[i][1] for i in range(0,4)], fault_type=data_stuck_at_LRS_noise[0][2], noise=True, fpath=font, save=True, labels=accuracy_labels)
+except IndexError:
+    print("Some necessary file is missing: complete accuracy plot cannot be produced.")
 
-# # Plotting accuracy curves for Type II (Stuck at HRS) fault
-# accuracy_curves_plotter(data_stuck_at_HRS[0][0], [data_stuck_at_HRS[i][1] for i in range(0,4)], fault_type=data_stuck_at_HRS[0][2], fpath=font, save=True, labels=accuracy_labels)
 
-# # Plotting accuracy curves for Type III (Stuck at LRS) fault
-# accuracy_curves_plotter(data_stuck_at_LRS[0][0], [data_stuck_at_LRS[i][1] for i in range(0,4)], fault_type=data_stuck_at_LRS[0][2], fpath=font, save=True, labels=accuracy_labels)
-
-# EDIT THIS BIT ACCORDINGLY IF STUFF WORKS
