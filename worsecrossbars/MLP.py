754e2db9cbb7bf2c87029abefdb564508f3e2433
Nicola Gigante <nicolagigante@outlook.com>
2021-10-31 17:27:19 +0000

    More integration work

--- worsecrossbars/MLP.py
+++ worsecrossbars/MLP.py
@@ -5,37 +5,37 @@ Worsecrossbars main module and entrypoint.
 
 import argparse
 import sys
+import signal
+import gc
+import platform
 from pathlib import Path
-from jsonschema import validate
-from jsonschema.exceptions import ValidationError
-from worsecrossbars.utilities import initial_setup
+from worsecrossbars.utilities import initial_setup, json_handlers
 from worsecrossbars.utilities import io_operations
+from worsecrossbars.utilities import Logging
+from worsecrossbars.utilities import MSTeamsNotifier
+
+def stop_handler(signum, frame):
+    """
+    A stop signal handler.
+    """
+    if command_line_args.log:
+        log.write(f"Simulation terminated unexpectedly. Got signal {signal.Signals(signum).name}.\nEnding.\n")
+        log.write(special="abruptend")
+    if command_line_args.teams:
+        number_hidden_layers = extracted_json["number_hidden_layers"]
+        hidden_layer = "hidden layer" if number_hidden_layers == 1 else "hidden layers"
+        fault_type = extracted_json["fault_type"]
+        teams.send_message(f"Simulation ({number_hidden_layers} {hidden_layer}, fault type {fault_type})" + \
+            f" terminated unexpectedly.\nGot signal {signal.Signals(signum).name}.\nEnding.", \
+             title="Simulation ended", color="b90e0a")
+    gc.collect()
+    sys.exit(0)
 
 def main():
     """
     main(command_line_args):
     The main function.
     """
-    json_schema = {
-        "type": "object",
-        "properties" : {
-            "HRS_LRS_ratio": {"type" : "integer"},
-            "number_of_conductance_levels": {"type": "integer"},
-            "excluded_weights_proportion" : {"type": "number"},
-            "number_hidden_layers": {"type": "integer"},
-            "fault_type": {"type": "string"},
-            "noise_variance": {"type": "number"},
-            "number_ANNs": {"type": "integer"},
-            "number_simulations": {"type": "integer"}
-        }
-    }
-    json_path = Path.cwd().joinpath(command_line_args.config)
-    extracted_json = io_operations.read_external_json(str(json_path))
-    try:
-        validate(extracted_json, json_schema)
-    except ValidationError as err:
-        print(f"{err.message}")
-        sys.exit(0)
     pass
 
 if __name__ == "__main__":
@@ -62,4 +62,23 @@ if __name__ == "__main__":
         initial_setup.main_setup(command_line_args.wipe_current)
         sys.exit(0)
     else:
+        json_path = Path.cwd().joinpath(command_line_args.config)
+        extracted_json = io_operations.read_external_json(str(json_path))
+        json_handlers.validate_json(extracted_json)
+        if command_line_args.log:
+            log = Logging(extracted_json["number_hidden_layers"], extracted_json["fault_type"], 
+            extracted_json["number_ANNs"], extracted_json["number_simulations"])
+            log.write(special="begin")
+        if command_line_args.teams:
+            webhook_url = io_operations.read_webhook()
+            teams = MSTeamsNotifier(webhook_url)
+            number_hidden_layers = extracted_json["number_hidden_layers"]
+            hidden_layer = "hidden layer" if number_hidden_layers == 1 else "hidden layers"
+            fault_type = extracted_json["fault_type"]
+            teams.send_message(f"Using parameters: {number_hidden_layers} {hidden_layer}," + \
+                f" fault type {fault_type}.", title="Started new simulation", color="028a0f")
+        signal.signal(signal.SIGINT, stop_handler)
+        signal.signal(signal.SIGTERM, stop_handler)
+        if platform.system() == "Darwin" or platform.system() == "Linux":
+            signal.signal(signal.SIGHUP, stop_handler)
         main()
---
+++ worsecrossbars/utilities/json_handlers.py
@@ -0,0 +1,30 @@
+"""
+An internal utility module to handle and validate JSON.
+"""
+import sys
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+def validate_json(extracted_json):
+    """
+    validate_json():
+    Validates a given JSON object against a standard schema.
+    """
+    json_schema = {
+        "type": "object",
+        "properties" : {
+            "HRS_LRS_ratio": {"type" : "integer"},
+            "number_of_conductance_levels": {"type": "integer"},
+            "excluded_weights_proportion" : {"type": "number"},
+            "number_hidden_layers": {"type": "integer"},
+            "fault_type": {"type": "string"},
+            "noise_variance": {"type": "number"},
+            "number_ANNs": {"type": "integer"},
+            "number_simulations": {"type": "integer"}
+        }
+    }
+    try:
+        validate(extracted_json, json_schema)
+    except ValidationError as err:
+        print(f"{err.message}")
+        sys.exit(0)
