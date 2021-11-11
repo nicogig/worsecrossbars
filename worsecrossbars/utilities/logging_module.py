5e5c84865197f1cd85bce65af780bea78a804ed7
Nicola Gigante <nicolagigante@outlook.com>
2021-11-11 18:12:05 +0000

    QOL Improvements

--- worsecrossbars/utilities/logging_module.py
+++ worsecrossbars/utilities/logging_module.py
@@ -16,18 +16,9 @@ class Logging:
     """
 
     def __init__ (self, extracted_json, output_folder):
-        log_number = 1
-        self.number_hidden_layers = extracted_json["number_hidden_layers"]
-        self.fault_type = extracted_json["fault_type"]
-        self.number_anns = extracted_json["number_ANNs"]
-        self.number_simulations = extracted_json["number_simulations"]
-        for _ in glob.glob(str(Path.home().joinpath("worsecrossbars",
-        "outputs", output_folder, "logs",
-        f"spruce_faultType{self.fault_type}_{self.number_hidden_layers}HL-?.log"))):
-            log_number += 1
+        self.simulation_parameters = extracted_json
         self.file_object = str(Path.home().joinpath("worsecrossbars",
-        "outputs", output_folder, "logs",
-        f"spruce_faultType{self.fault_type}_{self.number_hidden_layers}HL-{log_number}.log"))
+                                "outputs", output_folder, "logs", f"spruce.log"))
 
     def __call__ (self):
         pass
@@ -42,10 +33,8 @@ class Logging:
         """
         if special == "begin":
             write_string = f"----- Begin log {datetime.now().__str__()} -----\n" + \
-                        "Attempting simulation with following parameters:\n" + \
-                        f"number_hidden_layers: {self.number_hidden_layers}" + \
-                        f"\nfault_type: {self.fault_type}\nnumber_ANNs: {self.number_anns}" + \
-                        f"\nnumber_simulations: {self.number_simulations}\n\n"
+                            "Attempting simulation with following parameters:\n" + \
+                            f"{self.simulation_parameters}"
         elif special == "end":
             write_string = f"[{datetime.now().strftime('%H:%M:%S')}] Saved accuracies to file." + \
                     f" Ending.\n----- End log {datetime.now().__str__()} -----"
@@ -53,6 +42,6 @@ class Logging:
             write_string = f"[{datetime.now().strftime('%H:%M:%S')}] Abruptly Ending.\n"+ \
                     f"----- End log {datetime.now().__str__()} -----"
         else:
-            write_string = string
+            write_string = f"[{datetime.now().strftime('%H:%M:%S')}] " + string
         with open(self.file_object, "a", encoding="utf8") as file:
             file.write(f"[{datetime.now().strftime('%H:%M:%S')}] {write_string}\n")
