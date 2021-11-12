227e54637e733a1f58c036a684d6fa12409621d7
Lorenzo Bonito <lol99.bonito@gmail.com>
2021-11-12 15:05:14 +0000

    Improved accuracy curves argparser

--- worsecrossbars/plotting/plot_accuracy_curves.py
+++ worsecrossbars/plotting/plot_accuracy_curves.py
@@ -4,6 +4,7 @@ A plotting module used to generate accuracy curves.
 """
 
 import os
+import sys
 import argparse
 import pickle
 from pathlib import Path
@@ -18,11 +19,17 @@ def main():
 
     accuracies_objects_list = []
 
-    for filename in command_line_args.files:
+    for filetuple in command_line_args.files:
 
-        with open(str(Path.home().joinpath("worsecrossbars", "outputs", "accuracies", filename)),
-                  "rb") as file:
-            accuracies_objects_list.append(pickle.load(file))
+        try:
+            with open(str(Path.home().joinpath("worsecrossbars", "outputs", "accuracies",
+            f"accuracies_{filetuple[0]}_{filetuple[1]}HL_{filetuple[2]}NV.pickle")), "rb") as file:
+                accuracies_objects_list.append(pickle.load(file))
+        except FileNotFoundError:
+            print("The data you are attempting to plot does not exist. Please, check the command " +
+                  "line arguments are being entered in the format faultType_numberHiddenLayers_" +
+                  "noiseVariance, e.g. STUCKZERO_1_0.")
+            sys.exit(1)
 
     accuracy_curves_plotter(accuracies_objects_list, fpath=font, x_label=command_line_args.xlabel,
                             title=command_line_args.title, filename=command_line_args.filename)
@@ -33,16 +40,26 @@ if __name__ == "__main__":
     # Command line parser for input arguments
     parser = argparse.ArgumentParser()
 
+    # Adding a required positional argument to the command line parser. It should be noted that each
+    # string in the list below has the structure faultType_numberHiddenLayers_noiseVariance,
+    # e.g. STUCKZERO_1_0
+
+    parser.add_argument("files", nargs="+", metavar="FILES",
+        help="List of strings, each containing the parameters of a datafile to plot", type=str)
+
+    # Adding optional arguments to the command line parser
     parser.add_argument("-t", dest="title", metavar="PLOT_TITLE",
         help="Give the plot a specific title", type=str, default="")
     parser.add_argument("-x", dest="xlabel", metavar="PLOT_XLABEL",
         help="Give the plot a specific xlabel", type=str, default="")
-    parser.add_argument("--filename", dest="filename", metavar="PLOT_FILENAME",
+    parser.add_argument("-f", dest="filename", metavar="PLOT_FILENAME",
         help="Give the plot a specific filename", type=str, default="")
-    parser.add_argument("--files", dest="files", metavar="FILES",
-        help="List of strings with names of files to add to the plot", type=list, default=None)
 
     command_line_args = parser.parse_args()
+
+    # Turning list of strings in command_line_args.files into a list of tuples
+    command_line_args.files = [tuple(filestring.split("_")) for filestring in 
+                               command_line_args.files]
 
     # Importing LaTeX font for plots
     if os.path.exists(Path.home().joinpath("worsecrossbars", "utils", "cmunrm.ttf")):
