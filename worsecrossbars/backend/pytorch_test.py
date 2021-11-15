14527fe6a62adee47cd7cffb5fd739a9ed613b8a
Lorenzo Bonito <lol99.bonito@gmail.com>
2021-11-15 18:24:16 +0000

    Update pytorch_test.py

--- worsecrossbars/backend/pytorch_test.py
+++ worsecrossbars/backend/pytorch_test.py
@@ -8,7 +8,7 @@ device = torch.device("cuda:0")
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
+N, D_in, H, D_out = 64, 1000, 10000, 10
 
 # Create random input and output data
 x = torch.randn(N, D_in, device=device, dtype=dtype)
