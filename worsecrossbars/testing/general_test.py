16c0746ba1e76cbc0ec7c401b641bac43f934b57
Yousef Mahmoud <yousefosmahmoud@gmail.com>
2021-11-24 12:52:36 +0000

    Test commit

--- worsecrossbars/testing/general_test.py
+++ worsecrossbars/testing/general_test.py
@@ -1,3 +1,8 @@
 """A general testing module:
 Used to run a simple test case to verify automation
 To be deleted once automation is set up"""
+# "Importing needed packages"
+# from typing import List
+# "Calling the function upon which we want to conduct the tests"
+# "Implementing a general test case"
+# "Any code needed for the CI/Jenkins/Workflow on Github"
