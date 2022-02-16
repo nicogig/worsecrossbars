40e15121fea13a0595c8104f2b59bf4c017696ea
Nicola Gigante <nicolagigante@outlook.com>
2022-02-16 11:35:23 +0000

    Added mapping + nonlinearity in TF

---
+++ worsecrossbars/keras_legacy/mapping.py
@@ -0,0 +1,58 @@
+"""mapping:
+A backend module used to map memristive conductances to PyTorch synaptic layers, and vice versa.
+"""
+import tensorflow as tf
+
+@tf.function
+def weights_to_conductances(
+    weights: tf.Tensor,
+    G_off: float,
+    G_on: float,
+    mapping_style: str = "lowest",
+) -> tf.Tensor:
+    """Map PyTorch weights onto conductances.
+
+    Args:
+        weights: The original layer of weights as a Tensor.
+        G_off: A float representing the conductance of the devices in their OFF state.
+        G_on: A float representing the conductance of the devices in their ON state.
+        mapping_style: A string, either `lowest` or `average`.
+            Represents the mapping style to be utilised, where
+            `lowest` instructs the function to map according to the lowest possible conductance, and
+            `average` instructs the function to map the weights around an average conductance.
+
+    Returns:
+        conductance_layer: A PyTorch Tensor representing the mapped conductances, shape `m x 2n`.
+        maximum_weight: The max weight in the original weights layer.
+    """
+
+    maximum_weight = tf.math.reduce_max(tf.math.abs(weights))
+    scaling_fact_weight = (G_on - G_off) / maximum_weight
+    effective_cond = scaling_fact_weight * weights
+
+    if mapping_style == "lowest":
+
+        # Map according to lowest possible conductance
+        cond_pos = (
+            tf.math.maximum(effective_cond, tf.zeros(effective_cond.shape)) + G_off
+        )
+        cond_neg = (
+            tf.math.maximum(-effective_cond, tf.zeros(effective_cond.shape)) + G_off
+        )
+
+    elif mapping_style == "average":
+
+        # Map around the average conductance
+        avg_cond = (G_on + G_off) / 2
+        cond_pos, cond_neg = avg_cond + 0.5 * effective_cond, avg_cond - 0.5 * effective_cond
+
+    else:
+
+        raise ValueError("mapping_style parameter not valid.")
+
+    conductance_layer = tf.reshape(
+        tf.concat((cond_pos[..., tf.newaxis], cond_neg[..., tf.newaxis]), axis=-1),
+        [effective_cond.shape[0], -1],
+    )
+
+    return conductance_layer, maximum_weight
---
+++ worsecrossbars/keras_legacy/nonidealities.py
@@ -0,0 +1,93 @@
+"""nonidealities:
+A backend module used to simulate various memristive nonidealities.
+"""
+from typing import Tuple
+import tensorflow as tf
+
+class StuckAtValue:
+    """This class ..."""
+
+    def __init__(self, value: float, probability: float) -> None:
+        self.value = value
+        self.probability = probability
+        self.is_linearity_preserving = True
+
+    def alter_conductances(self, conductances: tf.Tensor) -> tf.Tensor:
+        """A method to alter conductances stored in a PyTorch Tensor.
+
+        Args:
+            conductances: A PyTorch tensor containing memristive conductances.
+
+        Returns:
+            altered_conductances: A PyTorch tensor containing the altered memristive conductances.
+        """
+
+        # Creating a mask of bools to alter a given percentage of conductance values
+        mask = (
+            tf.random.uniform(conductances.shape, dtype=tf.dtypes.float64) < self.probability
+        )
+        altered_conductances = tf.where(
+            mask, self.value, conductances
+        )
+
+        return altered_conductances
+
+
+class StuckDistribution:
+    """This class ..."""
+
+    def __init__(self) -> None:
+        pass
+
+
+class D2DVariability:
+    """This class ..."""
+
+    def __init__(self) -> None:
+        pass
+
+
+class IVNonlinear:
+    """This class ..."""
+
+    def __init__(self, V_ref: float, avg_gamma: float, std_gamma: float) -> None:
+        self.V_ref = V_ref
+        self.avg_gamma = avg_gamma
+        self.std_gamma = std_gamma
+        self.k_V = 2 * self.V_ref
+        self.is_linearity_preserving = False
+
+    def calc_currents(
+        self, voltages: tf.Tensor, conductances: tf.Tensor
+    ) -> Tuple[tf.Tensor, tf.Tensor]:
+        """This function implements Equation 9 from "Nonideality-Aware Training for Accurate and
+        Robust Low-Power Memristive Neural Networks" in order to compute output currents in a MCBA
+        affected by IV nonlinearities. The lowest possible value assigned to nonlinearity parameter
+        gamma is 2, in line with what is discussed in the paper.
+
+        Args:
+          voltages:
+          conductances:
+
+        Returns:
+          currents:
+          individual_currents:
+        """
+
+        # Generating a truncated normal distribution (with a low value of 2) describing the possible
+        # values taken on by the nonlinearity parameter gamma, which we shall sample from in order
+        # to assign a nonlinearity parameter to each memristor in the crossbar array.
+        gammas = tf.random.truncated_normal(
+            conductances.shape, self.avg_gamma, self.std_gamma
+        )
+
+        voltage_signs = tf.expand_dims(tf.sign(voltages), -1)
+        ohmic_currents = voltage_signs * self.V_ref * tf.expand_dims(conductances, 0)
+        v_v_ref_ratio = tf.expand_dims(tf.abs(voltages) / self.V_ref, -1)
+
+        log_gammas = tf.experimental.numpy.log2(gammas)
+
+        individual_currents = ohmic_currents * v_v_ref_ratio**log_gammas
+        currents = tf.math.reduce_sum(individual_currents, axis=1)
+
+        return currents, individual_currents
